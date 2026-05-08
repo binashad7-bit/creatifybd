35c76013378393a1e1dc91796a844a0cb81145ef
Bin Ashad <binashad7@gmail.com>
2026-05-08 16:58:29 +0600

    feat: maximize logo and favicon visibility

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -79,7 +79,7 @@ const Navbar = () => {
       <nav id="navbar" className={scrolled ? 'scrolled' : ''}>
         <div className="nav-container-inner">
           <Link to="/" className="nav-logo" data-cursor="Click" style={{ display: 'flex', alignItems: 'center', gap: '12px' }}>
-            <img src="/favicon.png" alt="CreatifyBD" style={{ height: '38px', width: 'auto' }} />
+            <img src="/favicon.png" alt="CreatifyBD" style={{ height: '52px', width: 'auto' }} />
             <span style={{ fontSize: '1.5rem', fontWeight: 800, letterSpacing: '-0.02em', color: '#000' }}>
               Creatify<span style={{ color: 'var(--red)' }}>BD</span>
             </span>
